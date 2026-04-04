65fd4a3beff9e8013b586a64253ab2964fa9909c
Jeong Hye in <97425636+h-cir@users.noreply.github.com>
2026-04-04 13:34:51 +0900

    docs: 소개 문구 수정 및 포트폴리오 콘텐츠 업데이트 (#3)
    * chore: 포트폴리오 템플릿 구조 및 기본 설정
    
    * docs: 포트폴리오 프로젝트 콘텐츠 및 이미지 추가
    
    * feat: 프로젝트 및 정보 섹션 렌더링 로직 개선
    
    * docs: 프로젝트 콘텐츠 및 이미지 추가, 오타 수정
    
    * docs: 소개 문구 및 포트폴리오 내용 업데이트

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -7,13 +7,14 @@ const Information = ({ information }: Pick<DataProps, "information">) => {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-2">
-        <h1 className="leading-[1.15]">
+        <h2 className="leading-[1.15]">
           {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
-          안녕하세요,
-          <br /> 클라우드 엔지니어{" "}
+          <p className="text-2xl font-bold">매장 운영을 설계하던 바리스타에서</p>
+          클라우드 아키텍처를 설계하는 엔지니어로
+          <br />성장하고 있는{" "}
           <span className="text-PRIMARY font-semibold">{information.name}</span>
           입니다.
-        </h1>
+        </h2>
         <div className="flex gap-1">
           {information.contact.map((contact) => (
             <ContactItem
